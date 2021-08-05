b5113e282ad78f0b164418e6bb8f47c6a860e3f7
Kavitha Srinimukesh <Kavitha.Srinimukesh@gmail.com>
2021-08-05 17:23:49 -0400

    Back to basics, Swift 4

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kavitha Srinimukesh.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Does any additional setup after loading the view
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Disposes of any resources that can be recreated
+    }
+
+
+}
+
